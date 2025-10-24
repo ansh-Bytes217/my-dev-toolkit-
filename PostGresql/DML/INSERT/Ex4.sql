ab1b4e57ccc28768c5463c49f91c5336805a9ac0
Ansh Malhotra <83686079+ansh-Bytes217@users.noreply.github.com>
2025-10-24 11:38:55 +0530

    Add SQL insert statement for countries table
    This SQL statement inserts a new row into the 'countries' table with specified values, including a NULL value.

---
+++ PostGresql/DML/INSERT/Ex4.sql
@@ -0,0 +1,3 @@
+-- This SQL statement inserts a new row into the 'countries' table with specified values, including a NULL value.
+
+INSERT INTO countries (country_id, country_name, region_id) VALUES('C3','UK',NULL);
