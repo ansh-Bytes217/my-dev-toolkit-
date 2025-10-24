075b4073da570eb3c71b3cf9c325b194fc6facba
Ansh Malhotra <83686079+ansh-Bytes217@users.noreply.github.com>
2025-10-24 11:29:52 +0530

    Create countries table with constraints
    This SQL statement creates a new table named 'countries' with columns for country ID, country name, and region ID, including constraints.

---
+++ PostGresql/Create Command/Ex8.sql
@@ -0,0 +1,9 @@
+-- This SQL statement creates a new table named 'countries' if it does not already exist,
+-- defining the structure and constraints for the table columns.
+
+CREATE TABLE IF NOT EXISTS countries ( 
+    COUNTRY_ID varchar(2) NOT NULL, -- Defines a column 'COUNTRY_ID' to store country IDs as strings with a maximum length of 2 characters. The column cannot contain NULL values.
+    COUNTRY_NAME varchar(40) NOT NULL, -- Defines a column 'COUNTRY_NAME' to store country names as strings with a maximum length of 40 characters. The column cannot contain NULL values.
+    REGION_ID decimal(10,0) NOT NULL, -- Defines a column 'REGION_ID' to store region IDs as decimal numbers with a precision of 10 digits and no decimal places. The column cannot contain NULL values.
+UNIQUE(COUNTRY_ID) -- Defines a unique constraint on the 'COUNTRY_ID' column, ensuring that each value in this column is unique across all rows.
+);
