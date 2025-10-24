4379ebaef921420e3fc0359613ba981cf0941d14
Ansh Malhotra <83686079+ansh-Bytes217@users.noreply.github.com>
2025-10-24 11:37:55 +0530

    Insert new country 'USA' into countries table
    This SQL statement inserts a new row into the 'countries' table with specified values for specific columns.

---
+++ PostGresql/DML/INSERT/Ex2.sql
@@ -0,0 +1,3 @@
+-- This SQL statement inserts a new row into the 'countries' table with specified values for specific columns.
+
+INSERT INTO countries (country_id, country_name) VALUES('C2','USA');
